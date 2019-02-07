ded486edb100de9e6e5499767b5ae3bbe8285321
johmy592 <johmy592@student.liu.se>
2019-02-07 15:11:37 +0100

    Now have a draft model that can make (bad) discoveries of hypernyms

--- evaluate.py
+++ evaluate.py
@@ -1,22 +1,33 @@
 from helpers import sigmoid
 import numpy as np
 
-def extract(q, transforms, embeddings, b, n=3):
+def extract(q, candidates, em, transforms, b, n=3):
     '''
-    Extract the n top ranked candidates
+    Extract the n top ranked hypernyms among candidates
+    input:
+    q: the embedding of the query
+    candidates: a list of candidate hypernym strings
+    em: dictionary containing all word add_embeddings
+    transforms: projection matrices
+    b: bias
     '''
-    
+    k = 24
     #print("Q: ", q,"\n")
     # p: All projections of q, list of k vectors
     p = [np.dot(transforms[i],q).T for i in range(k)]
-    
+
     # Normalize all projections
     for i in range(k):
         p[i] = p[i] / np.sqrt((np.sum(p[i]**2)))
-    
+
     # s: similarities of all projections of q, with all other terms
-    s = [(np.dot(p,embeddings[h]), h) for h in embeddings]
-    
+    """
+    Right now checks with all other embeddings (400k),
+    takes a while, but manageable
+    """
+    s = [(np.dot(p,em[h]), h) for h in candidates]
+    #s = [(np.dot(p,em[h]), h) for h in em]
+
     #print("Projections: ",p,"\n")
     #print("embeddings: ", embeddings,"\n")
     maxsims = [(_s[np.argmax(_s)], np.argmax(_s), h) for _s,h in s]
@@ -24,4 +35,3 @@ def extract(q, transforms, embeddings, b, n=3):
     sigmoids = [(sigmoid(np.add(_s,b[idx])), h) for _s,idx,h in maxsims]
     sigmoids.sort()
     return sigmoids[-n:]
-
--- main.py
+++ main.py
@@ -4,42 +4,12 @@ from training import *
 from evaluate import *
 from load_glove import load_embeddings
 
-def normalize_embeddings(embeddings):
-    print("Normalizing word embeddings\n")
-    for w in embeddings:
-        embeddings[w] = embeddings[w] / np.sqrt((np.sum(embeddings[w]**2)))
-    print("Done!\n")
 
-
-def replace_with_embedding(train_examples, embeddings, add_neg = 0):
-    embedding_triples = []
-    corresponding_words = []
-    possible_negatives = list(embeddings.keys())[:1000]
-    for q,h,t in train_examples:
-        if((not q in embeddings) or (not h in embeddings)):
-            continue
-        embedding_triples += [(embeddings[q],embeddings[h],t)]
-        embedding_triples += [(embeddings[q],embeddings[random.choice(possible_negatives)],0) for _ in range(add_neg)]
-    print("Created ", len(embedding_triples), " training examples with embeddings\n")    
-    return embedding_triples
-
-def read_train_examples(data_file, gold_file, num_examples = 1500, num_neg=3):
-    training_triples = []
-    gf = open(gold_file, 'r')
-    df = open(data_file, 'r')
-    for i in range(num_examples):
-        q = df.readline().split('\t')[0]
-        h = [_h.strip('\n') for _h in gf.readline().split('\t')]
-        training_triples += [(q,_h,1) for _h in h]
-    print("Generated ", len(training_triples), "positive training examples\n")
-    gf.close()
-    df.close()
-    return training_triples
 
 def test_train():
     data_file = "/home/johannes/thesis_code/ml_experimentation/data/1A.english.training.data.txt"
     gold_file = "/home/johannes/thesis_code/ml_experimentation/data/1A.english.training.gold.txt"
-    
+
     train_data = read_train_examples(data_file, gold_file)
 
 def test():
@@ -50,4 +20,4 @@ def test():
 
     # Set some parameters for the model
     d = 50
-    k = 24 
+    k = 24
