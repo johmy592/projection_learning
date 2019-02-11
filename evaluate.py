experimented with different data setups. Something appears to be wrong with how the gradient descent is done

evaluate.py
@@ -2,6 +2,7 @@ from helpers import sigmoid
 import numpy as np
 from globals import *
 
+
 def extract(q, candidates, em, transforms, b, n=3):
     '''
     Extract the n top ranked hypernyms among candidates
@@ -18,15 +19,19 @@ def extract(q, candidates, em, transforms, b, n=3):
     p = [np.dot(transforms[i],q).T for i in range(k)]
 
     # Normalize all projections
-    #for i in range(k):
-    #    p[i] = p[i] / np.sqrt((np.sum(p[i]**2)))
+    for i in range(k):
+        p[i] = p[i] / np.sqrt((np.sum(p[i]**2)))
+    #p = p/np.sqrt(np.sum(p**2))
+
 
     # s: similarities of all projections of q, with all other terms
     """
     Right now checks with all other embeddings (400k),
     takes a while, but manageable
     """
+    
     s = [(np.dot(p,em[h]), h) for h in candidates]
+    print(len(candidates))
     #s = [(np.dot(p,em[h]), h) for h in em]
 
     #print("Projections: ",p,"\n")
@@ -34,5 +39,6 @@ def extract(q, candidates, em, transforms, b, n=3):
     maxsims = [(_s[np.argmax(_s)], np.argmax(_s), h) for _s,h in s]
     #print(s)
     sigmoids = [(sigmoid(np.add(_s,b[idx])), h, idx) for _s,idx,h in maxsims]
+    #sigmoids = [(sigmoid(_s), h, idx) for _s,idx,h in maxsims]
     sigmoids.sort()
     return sigmoids[-n:]

load_glove.py
@@ -16,8 +16,13 @@ def load_wiki_embeddings(fname):
     fin = open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
     n, d = map(int, fin.readline().split())
     data = {}
+    i = 1
     for line in fin:
         tokens = line.rstrip().split(' ')
-        data[tokens[0]] = map(float, tokens[1:])
-    print("Done.",len(model)," words loaded!")
+        #data[tokens[0]] = map(float, tokens[1:])
+        data[tokens[0]] = np.array([float(val) for val in tokens[1:]]) 
+        if(i >= 400000):
+            break
+        i += 1
+    print("Done.",len(data)," words loaded!")
     return data

training.py
@@ -3,6 +3,7 @@ from globals import *
 import numpy as np
 import random
 
+
 def split_pos_neg(data):
     '''
     splits the training data into two sets, one with positive
@@ -17,8 +18,15 @@ def select_batch(pos_data, neg_data, num_pos=32, num_neg=32*5):
     Selects a random batch from all training data
     '''
     training_batch = []
+    negatives_to_add = []
     training_batch += random.sample(pos_data, num_pos)
-    training_batch += random.sample(neg_data, num_neg)
+    #for q,h,t in training_batch:
+    #    for q_,h_,t_ in neg_data:
+    #        if (q == q_).all():
+    #            negatives_to_add += [(q_,h_,t_)]
+    #training_batch += negatives_to_add
+    if neg_data:
+        training_batch += random.sample(neg_data, num_neg)
     return training_batch
 
 def one_iter(examples, transforms, b, learning_rate=0.01):
@@ -26,6 +34,7 @@ def one_iter(examples, transforms, b, learning_rate=0.01):
     Runs one iteration through all provided examples
     '''
     #maxnorm = 0
+    N = len(examples)
     total_loss = 0
     for q,h,t in examples:
         # project the query with all of the projection matrices
@@ -34,8 +43,10 @@ def one_iter(examples, transforms, b, learning_rate=0.01):
 
 
         # Normalize the projections
-        #for i in range(k):
-        #    p[i] = p[i] / np.sqrt((np.sum(p[i]**2)))
+
+        for i in range(k):
+            p[i] = p[i] / np.sqrt((np.sum(p[i]**2)))
+        #p = p/np.sqrt(np.sum(p**2))
 
         # compute similarities between all projections and the candidate
         s = np.dot(p,h)
@@ -43,6 +54,7 @@ def one_iter(examples, transforms, b, learning_rate=0.01):
         # Apply sigmoid function and find the transformation that resulted
         # in the closest projection to the candidate
         sigmoids = sigmoid(np.add(s,b))
+        #sigmoids = sigmoid(s)
         maxsim = np.argmax(sigmoids)
 
         # y: predicted similarity
@@ -55,10 +67,16 @@ def one_iter(examples, transforms, b, learning_rate=0.01):
             continue
         # Compute the loss and update the corresponding projection matrix
         # in accordance with gradient descent.
-        loss = t*np.log(y) + (np.subtract(1,t)*np.log(np.subtract(1,y)))
-        total_loss += loss
+        loss = t*np.log(y) - (np.subtract(1,t)*np.log(np.subtract(1,y)))
+        total_loss += -abs(loss)
 
         gradient = np.dot(x.T,loss)
+
+        #gradient = np.dot(np.subtract(x,h))
+        #if(t == 1):
+        #    gradient = np.dot(x.T,np.subtract(x,h))/N
+        #elif t == 0:
+        #    gradient = np.dot(x.T, np.subtract(x,h))*((-1)/N)
         #gradnorm = np.linalg.norm(gradient,2)
         #if(gradnorm > maxnorm):
         #    maxnorm = gradnorm
@@ -71,7 +89,8 @@ def one_iter(examples, transforms, b, learning_rate=0.01):
         #print("OLD: ", prev_theta ,"\n", "NEW: ", transforms[maxsim],"\n")
         #print("After: ", transforms[maxsim],"\n")
         #Update the bias
-        b_loss = y + (b[maxsim]-t)
+
+        b_loss = min(1,max(0,y + b[maxsim])) - t
         prev_b = b[maxsim]
         b[maxsim] =  np.subtract(prev_b, np.multiply(learning_rate, b_loss))
     #print("Maxnorm: ", maxnorm, "\n")
@@ -82,6 +101,7 @@ def train(embeddings, train_examples, num_iter=10, batching=False, learning_rate
     '''
     The main training algorithm.
     '''
+    rounds_per_batch = 8
     if batching:
         pos_examples,neg_examples = split_pos_neg(train_examples)
 
@@ -93,7 +113,8 @@ def train(embeddings, train_examples, num_iter=10, batching=False, learning_rate
     for it in range(num_iter):
         if batching:
             train_examples = select_batch(pos_examples,neg_examples)
-        loss = one_iter(train_examples, transforms,b, learning_rate)
+        for _ in range(rounds_per_batch):
+            loss = one_iter(train_examples, transforms,b, learning_rate)
         print("Loss, iter: ", loss/len(train_examples), it, "\n")
     #print(b)
     return (transforms, b)